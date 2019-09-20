6e2adb5fc77ada9f0b78f402a9665fcd4a6b232a
Đức Nghĩa <dArkgunz1998@gmail.com>
2019-09-20 22:24:27 +0700

    commit code

---
+++ DisplayAImage/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DisplayAImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pxbImage = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnChonFolder = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pxbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.pxbImage);
+            this.panel1.Location = new System.Drawing.Point(12, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 369);
+            this.panel1.TabIndex = 0;
+            // 
+            // pxbImage
+            // 
+            this.pxbImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pxbImage.Image = ((System.Drawing.Image)(resources.GetObject("pxbImage.Image")));
+            this.pxbImage.Location = new System.Drawing.Point(13, 0);
+            this.pxbImage.Name = "pxbImage";
+            this.pxbImage.Size = new System.Drawing.Size(747, 366);
+            this.pxbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pxbImage.TabIndex = 0;
+            this.pxbImage.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(347, 418);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(136, 49);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnChonFolder
+            // 
+            this.btnChonFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChonFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChonFolder.Location = new System.Drawing.Point(12, 418);
+            this.btnChonFolder.Name = "btnChonFolder";
+            this.btnChonFolder.Size = new System.Drawing.Size(135, 49);
+            this.btnChonFolder.TabIndex = 4;
+            this.btnChonFolder.Text = "Chọn folder";
+            this.btnChonFolder.UseVisualStyleBackColor = true;
+            this.btnChonFolder.Click += new System.EventHandler(this.btnChonFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 495);
+            this.Controls.Add(this.btnChonFolder);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Slide show image";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pxbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pxbImage;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnChonFolder;
+    }
+}
+
---
+++ DisplayAImage/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisplayAImage
+{
+    public partial class Form1 : Form
+    {
+        string openFileName;
+        OpenFileDialog folderBrowser = new OpenFileDialog();
+        Image img;
+        string pathDirectoryImg;
+        List<string> imageFiles = new List<string>();
+        int cnt = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            //var path = Application.StartupPath + "/img";
+
+            //if (Directory.Exists(path))
+            //{
+            //    var files = Directory.GetFiles(path, ".", SearchOption.AllDirectories);
+            //    if(files.Length > 0)
+            //    {
+            //        foreach (string filename in files)
+            //        {
+            //            if (Regex.IsMatch(filename, @".jpg|.png|.gif$"))
+            //            {
+            //                imageFiles.Add(filename);
+            //            }
+            //        }
+            //        pxbImage.Image = Image.FromFile(imageFiles[0]);
+            //    }
+
+            //}
+            pxbImage.AllowDrop = true;
+            pathDirectoryImg = Application.StartupPath + "/img";
+            if (Directory.Exists(pathDirectoryImg))
+            {
+                var files = Directory.GetFiles(pathDirectoryImg, "*.*", SearchOption.AllDirectories);
+                if (files.Length > 0)
+                {
+                    foreach (string filename in files)
+                    {
+                        if (Regex.IsMatch(filename, @".jpg|.png|.gif$"))
+                        {
+                            imageFiles.Add(filename);
+                        }
+                    }
+                    FileStream fileStream = new FileStream(imageFiles[0], FileMode.Open, FileAccess.Read);
+                    pxbImage.Image = Image.FromStream(fileStream);
+                    fileStream.Close();
+                }
+
+            }
+
+            //List<string> imageFiles = new List<string>();
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {                   
+            if(cnt > imageFiles.Count -1 )
+            {
+                cnt = 0;
+            }
+            else
+            {
+                pxbImage.Image = Image.FromFile(imageFiles[cnt]);
+                cnt++;
+            }
+            
+        }
+        Boolean checkClick = true;
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            
+            if (checkClick)
+            {
+                timer1.Enabled = true;
+                btnPlay.Text = "Stop";
+                checkClick = false;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                btnPlay.Text = "Play";
+                checkClick = true;
+            }
+           
+        }
+
+
+        private void btnChonFolder_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            OpenFileDialog openfileDialog = new OpenFileDialog();
+            openfileDialog.Title = "Chọn folder ảnh";
+            openfileDialog.Filter = "File anh(*.png; *.jpg)|*.png; *.jpg";
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                openFileName = openfileDialog.FileName;
+
+                try
+                {
+                    // Output the requested file in richTextBox1.
+                    // img = Image.FromFile(openFileName);
+                    var files = Directory.GetFiles(folderDialog.SelectedPath, "*.*", SearchOption.AllDirectories);
+
+                    imageFiles = new List<string>();
+                    int count = 1;
+                    foreach (string filename in files)
+                    {
+                        if (Regex.IsMatch(filename, @".jpg|.png|.gif$"))
+                        {
+                            imageFiles.Add(filename);
+                            //img = Image.FromFile(filename);
+
+                            //if (img != null)
+                            //{
+                            //    if (!Directory.Exists(pathDirectoryImg))
+                            //    {
+                            //        Directory.CreateDirectory(pathDirectoryImg);
+                            //    }
+                            //    string a = String.Format("/slide_{0}.png", count);
+                            //    img.Save(pathDirectoryImg + a);
+                            //    count++;
+
+                            //}
+                        }
+                    }
+                    if (imageFiles.Count > 0)
+                    {
+                       
+                        if (Directory.Exists(pathDirectoryImg))
+                        {
+                            var old_files = Directory.GetFiles(pathDirectoryImg, "*.*", SearchOption.AllDirectories);
+                            if (old_files.Length > 0)
+                            {
+                                foreach (string filename in old_files)
+                                {
+                                    if (Regex.IsMatch(filename, @".jpg|.png|.gif$"))
+                                    {
+
+                                        File.Delete(filename);
+                                    }
+                                }
+                            }
+                        }
+                        foreach (string filename in imageFiles)
+                        {
+                            img = Image.FromFile(filename);
+                            if (img != null)
+                            {
+                                if (!Directory.Exists(pathDirectoryImg))
+                                {
+                                    Directory.CreateDirectory(pathDirectoryImg);
+                                }
+
+                                string a = String.Format("/slide_{0}.png", count);
+                                img.Save(pathDirectoryImg + a);
+                                count++;
+
+                            }
+                        }
+                        FileStream fileStream = new FileStream(imageFiles[0], FileMode.Open, FileAccess.Read);
+                        pxbImage.Image = Image.FromStream(fileStream);
+                        fileStream.Close();
+                    }
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("The error is:"
+                                    + System.Environment.NewLine + exp.ToString() + System.Environment.NewLine);
+
+                }
+                Invalidate();
+                
+            }
+            btnPlay.Text = "Play";
+        }
+    }
+}
